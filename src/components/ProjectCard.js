c26193b8ab9f969be0cbc8cf2eaa9b29e000da7a
LloydC <lloydchambrier@gmail.com>
2025-02-01 09:56:12 +0100

    _id field update

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 
 // We are deconstructing props object directly in the parentheses of the function
-function ProjectCard ( { title, description, _id } ) {
+function ProjectCard ( { title, description, id } ) {
   
   return (
     <div className="ProjectCard card">
-      <Link to={`/projects/${_id}`}>
+      <Link to={`/projects/${id}`}>
         <h3>{title}</h3>
       </Link>
       <p style={{ maxWidth: "400px" }}>{description} </p>
